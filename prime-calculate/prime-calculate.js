29e3e55135f59eb16977138c26eae7cea6a9b262
Carlos <carlosmontoya@outlook.com>
2018-11-18 08:40:55 -0500

    added commentaries to explain how the program works

--- prime-calculate/prime-calculate.js
+++ prime-calculate/prime-calculate.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
+//program to print all primes smaller than or equal to 
+// n using Sieve of Eratosthenes 
+
+
+// Create a boolean array "arrayNumbers[0..n]" and initialize 
+// all entries in it as true. A value in arrayNumbers[i] will 
+// finally be false if i is Not a prime, else true. 
 
 let arrayNumbers = [];
 
 const listPrimes = (n) => {
-    let arrayPrimeNumbers = [];
     for (var i = 1; i < n; i++) {
         arrayNumbers[i] = true;
     }
 
     for (var p = 2; p < p * p; p++) {
 
+        // If arrayNumber[p] is not changed, then it is a prime 
+
         if (arrayNumbers[p] == true) {
+
+            // Update all multiples of p 
 
             for (var i = p * 2; i <= n; i += p) {
                 arrayNumbers[i] = false;
